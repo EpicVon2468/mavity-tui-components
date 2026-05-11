d96c160ab2c201f97dff9bcc04ec57b258ecb8d4
Mavity The Madity <thomaschad0909@gmail.com>
2026-05-11 20:36:12 +1000

    Defaults for App

--- src/app.rs
+++ src/app.rs
@@ -7,11 +7,17 @@ pub const trait App {
 
 	fn was_key_down(&self, key: KeyCode) -> bool;
 
-	fn should_exit(&self) -> bool;
+	fn should_exit(&self) -> bool {
+		self.was_key_down(KeyCode::Char(':')) && self.is_key_down(KeyCode::Char('q'))
+	}
 
-	fn should_shl(&self) -> bool;
+	fn should_shl(&self) -> bool {
+		self.is_key_down(KeyCode::Left) || self.is_key_down(KeyCode::BackTab)
+	}
 
-	fn should_shr(&self) -> bool;
+	fn should_shr(&self) -> bool {
+		self.is_key_down(KeyCode::Right) || self.is_key_down(KeyCode::Tab)
+	}
 
 	fn submit(&mut self, event: Self::Event, closure: fn()) -> bool;
 }
